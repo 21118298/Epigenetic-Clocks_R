92d9c63fbe0c615f0dd302f59fa9f6d715ef99e9
21118298 <106642775+21118298@users.noreply.github.com>
2022-06-04 21:32:29 +0800

    Update 1.1 split_chr_test.R

--- 1.1 split_chr_test.R
+++ 1.1 split_chr_test.R
@@ -1,6 +1,7 @@
   for (i in 1:22){
   result <- split(x, x$chr)
-  #assign(paste0(file,"_chr",i,sep=""),clump_data(result[[i]], clump_p1 = 5e-08, clump_p2 = 5e-08)
+  #x <- rbind(assign(paste0(file,"_chr",i,sep=""),clump_data(result[[i]], clump_p1 = 5e-08, clump_p2 = 5e-08))
+  write.table(result[[i]], file = paste0(file,"_chr",i,sep=""), quote = FALSE)
   y <- clump_data(result[[i]], clump_p1 = 5e-08, clump_p2 = 5e-08)
   write.table(y, file = paste0(file,"_chr",i,sep=""), quote = FALSE)
   }
