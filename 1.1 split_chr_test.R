6f0b90fa34f2df1f11bab77276d70f9f0923b630
21118298 <106642775+21118298@users.noreply.github.com>
2022-06-11 14:42:58 +0800

    Update 1.1 split_chr_test.R

--- 1.1 split_chr_test.R
+++ 1.1 split_chr_test.R
@@ -1,8 +1,23 @@
-  for (i in 1:22){
-  result <- split(x, x$chr)
-  #x <- rbind(assign(paste0(file,"_chr",i,sep=""),clump_data(result[[i]], clump_p1 = 5e-08, clump_p2 = 5e-08))
-  write.table(result[[i]], file = paste0(file,"_chr",i,sep=""), quote = FALSE)
-  y <- clump_data(result[[i]], clump_p1 = 5e-08, clump_p2 = 5e-08)
-  write.table(y, file = paste0(file,"_chr",i,sep=""), quote = FALSE)
+for (i in 1:22){                   #先查看这个文件染色体列unique值，发现没有X，Y。因此循环从1到22
+  result <- split(x, x$chr)          #由于clump_data,timeout issue,解决办法按染色体分割
+  #x <- rbind(assign(paste0(file,"_chr",i,sep=""),subset(result[[i]], pval.exposure < 5e-08)))
+  write.table(result[[i]], file = paste0("abc_chr",i,sep=""), quote = FALSE, row.names = FALSE) #按染色体出文件，备用
+  newdata <-subset(result[[i]], pval.exposure < 5e-08)                                          #直接clump_data()染色体文件报错，因此先筛选pvalue<5e-08,再clump
+  y <- clump_data(newdata, clump_p1 = 5e-08, clump_p2 = 5e-08)                                  #clump
+  write.table(y, file = paste0(file,"_chr",i,sep=""), quote = FALSE, row.names = FALSE)         #出文件，备用
   }
-  z <- rbind(...)
+  z <- rbind(chr6,chr17,chr10) 
+
+  最后的结果是4条 GrimAge，与文献所述相同，结果如下：
+
+          SNP chr.exposure pos.exposure effect_allele.exposure other_allele.exposure eaf.exposure beta.exposure se.exposure 
+1   rs4065321           17     38143548                      T                     C       0.5333       -0.1703      0.0296
+11  rs9386796            6    109618704                      T                     C       0.4597        0.1983      0.0294
+23   rs887466            6     31143511                      A                     G       0.3815       -0.1928      0.0310
+12 rs17094148           10    101280279                      A                     G       0.7067       -0.1800      0.0323
+
+   pval.exposure samplesize.exposure               exposure mr_keep.exposure pval_origin.exposure id.exposure
+1      8.776e-09               32420 DNA_methylation_ageing             TRUE             reported     GrimAge
+11     1.640e-11               32418 DNA_methylation_ageing             TRUE             reported     GrimAge
+23     5.092e-10               30674 DNA_methylation_ageing             TRUE             reported     GrimAge
+12     2.547e-08               31838 DNA_methylation_ageing             TRUE             reported     GrimAge
