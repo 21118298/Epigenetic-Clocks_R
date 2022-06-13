ef06ba68eb40427b99ae4a8735b605247164288d
21118298 <106642775+21118298@users.noreply.github.com>
2022-06-13 15:37:28 +0800

    Update 2.2 FinnGen_Result.R

--- 2.2 FinnGen_Result.R
+++ 2.2 FinnGen_Result.R
@@ -12,14 +12,17 @@ write.table(nenwdata, file = paste0("finngen_lung_filter.txt"), quote = FALSE, r
 
 结果意义：利用LDlinkR-LDproxy找到Exposore GrimAge4个SNP在所有Finngen5个癌种的GWASdataset都存在的proxy。(5个癌种breast,prostate, colorectal, ovarian and lung cancers)
 
-结果如下1，3，5，7为original，2，4，6，8为proxy：
-    RS_Number           Coord Alleles    MAF Distance Dprime     R2 Correlated_Alleles RegulomeDB Function   original
-1   rs4065321  chr17:38143548   (C/T) 0.4642        0      1 1.0000            C=C,T=T          5     <NA>  rs4065321
-4  rs12453764  chr17:38144187   (T/C) 0.4642      639      1 1.0000            C=T,T=C          7     <NA>  rs4065321
-11  rs9386796  chr6:109618704   (C/T) 0.4284        0      1 1.0000            C=C,T=T         1f     <NA>  rs9386796
-2   rs5005289  chr6:109617673   (C/G) 0.4284    -1031      1 1.0000            C=C,T=G          4     <NA>  rs9386796
-12   rs887466   chr6:31143511   (G/A) 0.4076        0      1 1.0000            G=G,A=A         1b     <NA>   rs887466
-21  rs1265155   chr6:31143694   (G/A) 0.3877      183      1 0.9203            G=G,A=A         2b     <NA>   rs887466
-13 rs17094148 chr10:101280279   (A/G) 0.3022        0      1 1.0000            A=A,G=G          4     <NA> rs17094148
-22 rs11190133 chr10:101278725   (C/T) 0.3022    -1554      1 1.0000            A=C,G=T          5     <NA> rs17094148
+结果如下: GrimAge的4个snp在finngen dataset中都能找到不需要proxy。
+  chr.outcome pos.outcome other_allele.outcome effect_allele.outcome        SNP pval.outcome beta.outcome se.outcome eaf.outcome
+1           6    31175734                    G                     A   rs887466    0.0482259   -0.0522429  0.0264471    0.472498
+2           6   109297501                    C                     T  rs9386796    0.2056880    0.0334395  0.0264237    0.505749
+3          10    99520522                    A                     G rs17094148    0.8839680    0.0039362  0.0269711    0.392376
+4          17    39987295                    C                     T  rs4065321    0.1607330    0.0370189  0.0263929    0.480417
+                                     outcome mr_keep.outcome pval_origin.outcome id.outcome
+1 lung cancer (excluding cancer in controls)            TRUE            reported     zIV0uA
+2 lung cancer (excluding cancer in controls)            TRUE            reported     zIV0uA
+3 lung cancer (excluding cancer in controls)            TRUE            reported     zIV0uA
+4 lung cancer (excluding cancer in controls)            TRUE            reported     zIV0uA
+
+尽管如此，还是运行一下该步骤的代码。结果如下：
 
