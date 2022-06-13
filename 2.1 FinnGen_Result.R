e144a79630f646e74c60e6fadaa1d1d5b6db5053
21118298 <106642775+21118298@users.noreply.github.com>
2022-06-13 20:06:10 +0800

    Update 2.1 FinnGen_Result.R

--- 2.1 FinnGen_Result.R
+++ 2.1 FinnGen_Result.R
@@ -13,18 +13,11 @@ write.table(nenwdata, file = paste0("finngen_lung_filter.txt"), quote = FALSE, r
 结果意义：利用LDlinkR-LDproxy找到Exposore GrimAge4个SNP在所有Finngen5个癌种的GWASdataset都存在的proxy。(5个癌种breast,prostate, colorectal, ovarian and lung cancers)
 
 结果如下: GrimAge的4个snp在finngen dataset中都能找到不需要proxy。
-  chr.outcome pos.outcome other_allele.outcome effect_allele.outcome        SNP pval.outcome beta.outcome se.outcome eaf.outcome
-1           6    31175734                    G                     A   rs887466    0.0482259   -0.0522429  0.0264471    0.472498
-2           6   109297501                    C                     T  rs9386796    0.2056880    0.0334395  0.0264237    0.505749
-3          10    99520522                    A                     G rs17094148    0.8839680    0.0039362  0.0269711    0.392376
-4          17    39987295                    C                     T  rs4065321    0.1607330    0.0370189  0.0263929    0.480417
-                                     outcome mr_keep.outcome pval_origin.outcome id.outcome
-1 lung cancer (excluding cancer in controls)            TRUE            reported     zIV0uA
-2 lung cancer (excluding cancer in controls)            TRUE            reported     zIV0uA
-3 lung cancer (excluding cancer in controls)            TRUE            reported     zIV0uA
-4 lung cancer (excluding cancer in controls)            TRUE            reported     zIV0uA
-
-
+  chr.outcome pos.outcome other_allele.outcome effect_allele.outcome        SNP pval.outcome beta.outcome se.outcome eaf.outcome outcome mr_keep.outcome pval_origin.outcome id.outcome
+1           6    31175734                    G                     A   rs887466    0.0482259   -0.0522429  0.0264471    0.472498 lung cancer (excluding cancer in controls)            TRUE            reported     zIV0uA
+2           6   109297501                    C                     T  rs9386796    0.2056880    0.0334395  0.0264237    0.505749 lung cancer (excluding cancer in controls)            TRUE            reported     zIV0uA
+3          10    99520522                    A                     G rs17094148    0.8839680    0.0039362  0.0269711    0.392376 lung cancer (excluding cancer in controls)            TRUE            reported     zIV0uA
+4          17    39987295                    C                     T  rs4065321    0.1607330    0.0370189  0.0263929    0.480417 lung cancer (excluding cancer in controls)            TRUE            reported     zIV0uA
 
 
 尽管如此，还是运行一下该步骤的代码。1-4是exp_dat对应的snp，5-8是proxy的format_data后的结果。结果如下：
