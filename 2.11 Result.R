8e864a8761e134dfd6b7cbe5ba72d337333eef7b
21118298 <106642775+21118298@users.noreply.github.com>
2022-06-13 10:43:25 +0800

    Create 2.11 Result.R

---
+++ 2.11 Result.R
@@ -0,0 +1,10 @@
+结果：
+    RS_Number           Coord Alleles    MAF Distance Dprime     R2 Correlated_Alleles RegulomeDB Function   original
+1   rs4065321  chr17:38143548   (C/T) 0.4642        0      1 1.0000            C=C,T=T          5     <NA>  rs4065321
+4  rs12453764  chr17:38144187   (T/C) 0.4642      639      1 1.0000            C=T,T=C          7     <NA>  rs4065321
+11  rs9386796  chr6:109618704   (C/T) 0.4284        0      1 1.0000            C=C,T=T         1f     <NA>  rs9386796
+2   rs5005289  chr6:109617673   (C/G) 0.4284    -1031      1 1.0000            C=C,T=G          4     <NA>  rs9386796
+12   rs887466   chr6:31143511   (G/A) 0.4076        0      1 1.0000            G=G,A=A         1b     <NA>   rs887466
+21  rs1265155   chr6:31143694   (G/A) 0.3877      183      1 0.9203            G=G,A=A         2b     <NA>   rs887466
+13 rs17094148 chr10:101280279   (A/G) 0.3022        0      1 1.0000            A=A,G=G          4     <NA> rs17094148
+22 rs11190133 chr10:101278725   (C/T) 0.3022    -1554      1 1.0000            A=C,G=T          5     <NA> rs17094148
