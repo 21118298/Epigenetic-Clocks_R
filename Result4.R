17ad39ebb3234c96c2dddfa29c0e872627d1ba4e
21118298 <106642775+21118298@users.noreply.github.com>
2022-06-14 10:39:02 +0800

    Create Result4.R

---
+++ Result4.R
@@ -0,0 +1,4 @@
+Meta-analysis 从相关研究中整合定量数据，并产生能够总结研究整体的结果。Fixed effect meta-analysis假定所有研究都估计固定的treatment effect。
+fixed effect假定所有研究都估计出相同的治疗效果，只是因为取样的偶然（chance）差异导致观察到的treatment effect不同。
+合并之前，首先应该进行异质性检验，保证现有的各独立研究间的结果的不同仅仅是由于抽样误差造成的。
+若同时如何I2<50%和P≥0.1时，纳入文献被认为是同质的，采用固定效应模型（fixed effect model）分析
