20d2e61f95c70bc29e3a48e726b10fc62a1e4b1d
21118298 <106642775+21118298@users.noreply.github.com>
2022-06-14 10:09:39 +0800

    Update 3.1 run_2MR_result.R

--- 3.1 run_2MR_result.R
+++ 3.1 run_2MR_result.R
@@ -8,4 +8,5 @@ id.exposure id.outcome                                    outcome               
 结果意义：
 MR分析输入exposure和outcome文件，Exposure文件为遗传变异与暴露因素的gwas结果，Outcome文件为遗传变异与结局变量的gwas结果。这两个文件我们分别在step1和2format好。
 Harmonise data主要目的用于将SNP位点统一调整成正链，并且根据allele和频率判断两个gwas结果中的SNP位点是否一致，不一致的进行去除。
-通过4种算法计算出MR结果，主要看IVW的结果，其他作为稳定性和一致性的参考。b相当于OR值，是正向或者反向量化的一个指标。
+通过4种算法计算出MR结果不同，我们主要看你方差加权法IVW的结果，其他作为稳定性和可靠性的参考。b相当于OR值，是正向或者反向量化的一个指标。
+IVW MR法是假设在所有遗传变异都是有效工具变量的基本前提下进行的有效分析，具有较强的因果关系检测额能力。
