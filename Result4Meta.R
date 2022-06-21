ffa3989772803e8acfb74499ad835d3b7f062742
21118298 <106642775+21118298@users.noreply.github.com>
2022-06-21 19:51:18 +0800

    Update Result4Meta.R

--- Result4Meta.R
+++ Result4Meta.R
@@ -1,6 +1,5 @@
 Meta-analysis 从相关研究中整合定量数据，并产生能够总结研究整体的结果。
 fixed effect假定所有研究都估计出相同的治疗效果，只是因为取样的偶然（chance）差异导致观察到的treatment effect不同。
-合并之前，首先应该进行异质性检验，保证现有的各独立研究间的结果的不同仅仅是由于抽样误差造成的。
 
 # Metagen（GrimAge）结果
 Number of studies combined: k = 6  #本次纳入meta分析的研究（或效应量）有6个（lung cancer两个），因此k=6。
